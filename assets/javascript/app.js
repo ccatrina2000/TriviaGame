4c61c3033af7212dc91eb1604b10b7487b004782
Cornel Catrina <catrina.cornel@gmail.com>
2017-07-24 21:46:49 -0700

    Timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,20 @@
-console.log("10");
+
+//Timer Countdown
+
+var timeRemaining = 30;
+function timeTickDown() {
+	clock = setInterval(thirtySeconds, 1000);
+	function thirtySeconds () {
+		if (timeRemaining === 0) {
+			clearInterval(clock);
+		}
+		if (timeRemaining > 0) {
+			timeRemaining--;
+		document.getElementById("timeRemaining").textContent = timeRemaining;	
+		}
+	}
+}
+
+timeTickDown();
+
+
